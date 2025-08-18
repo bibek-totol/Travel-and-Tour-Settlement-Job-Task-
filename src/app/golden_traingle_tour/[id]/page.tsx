57ce5dce6673893648b035695489fdcbe71e98de
bibek-totol <bibektotol@gmail.com>
2025-08-19 04:24:03 +0600

    try to solve typescript error on production

--- src/app/golden_traingle_tour/[id]/page.tsx
+++ src/app/golden_traingle_tour/[id]/page.tsx
@@ -17,7 +17,7 @@ export async function generateStaticParams() {
 
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const { id } = await params;
+  const { id } =  params;
 
   const data: Tour[] = await alltourapidata();
 
