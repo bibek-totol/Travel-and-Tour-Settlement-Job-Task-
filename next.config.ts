e846c7d2a1a7f7d7b27624e90bf9ab993952dbe5
bibek-totol <bibektotol@gmail.com>
2025-08-19 14:39:46 +0600

    &

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "explorerajasthantours.com", 
-        pathname: "/wp-content/uploads/**",
-        
-
-      },
-    ],
+    domains: ["explorerajasthantours.com"],
   },
 };
 
