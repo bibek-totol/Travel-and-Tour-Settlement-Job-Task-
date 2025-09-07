47145688b258f983f50433404e6253d68bb72705
bibek-totol <bibektotol@gmail.com>
2025-09-07 18:05:21 +0600

    Animated Chat button

--- src/app/components/FloatingButtons.tsx
+++ src/app/components/FloatingButtons.tsx
@@ -8,44 +8,34 @@ export default function FloatingButtons() {
       
       <motion.div
         className="fixed left-10 bottom-1/13 z-50 flex items-center space-x-2 cursor-pointer"
+        animate={{ scale: [0.8,0.8,0.8,1.20,1.20,1.20,0.8], rotate:[0,0,-10,10,5,10,0] }} 
+        transition={{
+          duration: 3,
+          repeat: Infinity,
+          ease: "easeInOut",
+          repeatDelay:1,
+          times: [0, 0.2, 0.45, 0.48, 0.5, 0.52, 1]
+
+        
+        }}
       >
         
-        <motion.div
-          animate={{ scale: [1, 1.20, 0.95, 1] }} 
-          transition={{
-            duration: 4,
-            repeat: Infinity,
-            ease: "easeInOut"
-          }}
+        <div
+         
           className="bg-orange-500 text-white rounded-full p-4 shadow-lg flex items-center justify-center hover:bg-orange-600"
         >
           <User className="w-6 h-6" />
-        </motion.div>
+        </div>
 
         {/* Label */}
-        <motion.div
-          animate={{
-            scale: [1, 1.20, 0.95, 1],            
-            rotate: [-10, 10, 20, -10]              
-          }}
-          transition={{
-            scale: {
-              duration: 4,
-              repeat: Infinity,
-              ease: "easeInOut"
-            },
-            rotate: {
-              duration: 4,
-              times: [0, 0.3, 0.6, 1], 
-              repeat: Infinity,
-              ease: "easeInOut"
-            }
-          }}
+        <div
+          
           className="bg-gray-700 text-white text-sm font-medium px-3 py-1 rounded shadow-md -ml-2 origin-left"
         >
           May I Help You!
-        </motion.div>
+        </div>
       </motion.div>
+
 
       
       <div className="fixed right-4 bottom-1/13 z-50">
